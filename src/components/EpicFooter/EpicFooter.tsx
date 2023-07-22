0c1dee44aa8db2de9e6c1d36984753543860fc4f
stijuh <stije.it@gmail.com>
2023-07-22 20:05:09 +0200

    fixed css??? again????????

--- src/components/EpicFooter/EpicFooter.tsx
+++ src/components/EpicFooter/EpicFooter.tsx
@@ -4,22 +4,19 @@ import "./EpicFooter.scss"
 function EpicFooter() {
     return (
         <footer>
-            {/*<span className={"date"}>@2023</span>*/}
-
-            <div className={"foot"}>
-                <div className="g-container">
-                    <div className="g-wrap">
-                        <div className="g-footer">
-                            {(() => {
-                                return Array.from(
-                                    { length: 50 },
-                                    (_, i) => (<div key={"bubble-" + i} className="g-bubble"></div>)
-                                );
-                            })()}
-                        </div>
+            <div className="g-container">
+                <div className="g-wrap">
+                    <div className="g-footer">
+                        {(() => {
+                            return Array.from(
+                                { length: 50 },
+                                (_, i) => (<div key={"bubble-" + i} className="g-bubble"></div>)
+                            );
+                        })()}
                     </div>
                 </div>
             </div>
+            <p className={"date"}>2023</p>
 
             <a className={"studioStoyLink"} href="https://studiostoy.nl" target="_blank">$tudio $toy</a>
         </footer>
