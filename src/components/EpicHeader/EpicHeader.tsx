09215ae010623ca3eb00df2e96bba10f9c71b718
stijuh <stije.it@gmail.com>
2023-07-23 00:31:47 +0200

    Cave Johnson did our introduction. He did call me a bean counter but that's fine

--- src/components/EpicHeader/EpicHeader.tsx
+++ src/components/EpicHeader/EpicHeader.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 import "./EpicHeader.css"
 import logo from "../../logo.svg";
+import InfoMenu from "../InfoMenu/InfoMenu";
 
-function EpicHeader() {
-    return (
-        <header>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p className="text">Because time is money!</p>
-        </header>
-    );
+interface InfoMenuModalState {
+    isOpen: boolean
 }
 
-export default EpicHeader;
+export default class EpicHeader extends Component<{}, InfoMenuModalState> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggleInfo = () => {
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen
+        }));
+    };
+
+    closeInfo = () => {
+        this.setState(() => ({
+            isOpen: false
+        }));
+    };
+
+    render() {
+        const {isOpen} = this.state;
+
+        return (
+            <>
+                <header>
+                    <div className={"logo"} onClick={this.toggleInfo}>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </div>
+                    <p className="text">Because time is money!</p>
+                </header>
+
+                <div className={"info-menu" + (isOpen ? " active" : "")} onClick={this.closeInfo}>
+                    <InfoMenu handleClose={this.closeInfo}/>
+                </div>
+            </>
+        );
+    }
+}
---
+++ src/components/InfoMenu/InfoMenu.tsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import "./InfoMenu.css"
+
+interface CloseHandler {
+    handleClose: () => void
+}
+
+export default class InfoMenu extends Component<CloseHandler, {}> {
+    constructor(props: CloseHandler) {
+        super(props);
+    }
+
+    componentDidMount() {
+        // Add event listener when the component mounts
+        window.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        // Remove event listener when the component unmounts to prevent memory leaks
+        window.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown = (event: KeyboardEvent) => {
+        // Check if the pressed key is "Esc" (key code 27)
+        if (event.key === "Escape") {
+            // Call the function to close the modal
+            this.props.handleClose();
+        }
+    };
+
+    render() {
+        return (
+            <>
+                <div className={"info-modal"}>
+                    <div className={"info-modal-content"}>
+                        <h1>Coin Counter</h1>
+
+                        <ul className={"reasons-to-enlist"}>
+                            <li>Are you tired of slogging through mountains of mundane tasks like some kind of unfortunate desk-bound Olympian?</li>
+                            <li>Do you crave a monetary carrot dangling in front of your nose, urging you to rise above the sea of mediocrity?</li>
+                            <li>Are you an aspiring millionaire or a penny-pincher on the brink of a financial revolution?</li>
+                        </ul>
+
+                        <p className={"listen-up"}>If the above applies to you, then listen up, because we've got the answer you've been waiting for!</p>
+
+                        <p className={"information"}>
+                            Welcome, all you monetary-focused bean counters, to the spectacular world of <strong>Coin Counter! </strong>
+                            This ain't your run-of-the-mill mundane accountancy application, Oh no!
+                            It's a glorious symphony of capitalism and productivity,
+                            designed to keep those gears turning while you rake in the dough!
+
+                            <br/>
+                            <br/>
+
+                            Say goodbye to your mediocre existence and hello to the life of a monetarily stimulated titan!
+                            With the <strong>Coin Counter</strong>, you're not just some ordinary war hero, no sir!
+                            You're a profit-pursuing, number-crunching, task-conquering dynamo!
+
+                            <br/>
+                            <br/>
+
+                            Besides, who needs peace and tranquility when you can have constant reminders of your financial inadequacy?
+
+                            <br/>
+                            <br/>
+
+                            Cave Johnson, we're done here.
+                        </p>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
